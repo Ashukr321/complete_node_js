5cfb05404e5d0fa20921d648c712b418ff52a801
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-03 20:55:46 +0530

    bloacking and non bloacking code

---
+++ introduction_to_node_js_and_npm/bloacking_and_non_blocking.js
@@ -0,0 +1,9 @@
+// blocking and non bloacking asynchronous js 
+
+// synchronous means simple each line of the code 
+// is executed one by one line by line 
+
+// asynchorouns is the non bloacking code 
+// this used more callback function 
+
+// asynchronous js is work on single threads 
