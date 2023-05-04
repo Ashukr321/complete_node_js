921dde36c8520192052cfc909b1b4a5dd5a7eaf1
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-04 20:21:13 +0530

    node js process and thread

---
+++ how_node_js_work/process_and_thread.js
@@ -0,0 +1,9 @@
+// what is the process in the node js 
+
+// node js process 
+// programme in execution know as process 
+
+// process is executed in the thread 
+// thread is the sequence of the instruction 
+
+// node run in just one thread in  the node js 
